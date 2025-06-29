5aa7564a42e0bea9c3e2a64f7c46e6870e884356
datafedai <datafedai@gmail.com>
2025-06-29 16:30:44 -0400

    thermometer movement added to the script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public GameObject thermometer; // themometer controller
-    public const float movementSpeed = 1f;
+    public float movementSpeed = 1f;
     //public InputSystem_Actions inputActions;
+    InputAction moveAction;
+
     private void OnEnable()
     {
         //inputActions.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
@@ -15,12 +17,22 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Time.deltaTime * 1.1f, 0, 0);
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        //transform.Translate(Time.deltaTime * 1.1f, 0, 0);
+        if (moveValue == new Vector2(1,0))
+        {
+            transform.Translate(Time.deltaTime * movementSpeed, 0, 0);
+
+        }
+        else if (moveValue == new Vector2(-1,0))
+        {
+             transform.Translate(-Time.deltaTime * movementSpeed, 0, 0);
+        }
     }
 }
